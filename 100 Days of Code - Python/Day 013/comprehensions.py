365343803f42301b9d3e6e7efbafb7d7e209d8d7
Ariadne Vieira <64850642+Aaaririri@users.noreply.github.com>
2021-03-19 18:47:13 -0300

    Add files via upload

---
+++ 100 Days of Code - Python/Day 013/comprehensions.py
@@ -0,0 +1,5 @@
+num = [1,2,3,4,5]
+print([numero for numero in num])
+print([numero for numero in num if numero % 2 == 0])
+print([numero for numero in num if numero % 2 != 0])
+print([numero ** 2 for numero in num])
