6d1f03e44a91523af6c39530730b61b4c2f8fa57
Ariadne Vieira <64850642+Aaaririri@users.noreply.github.com>
2021-03-23 15:18:42 -0300

    Add files via upload

--- 100 Days of Code - Python/Day 017/quiz game/quiz_brain.py
+++ 100 Days of Code - Python/Day 017/quiz game/quiz_brain.py
@@ -14,19 +14,24 @@ class QuizBrain:
     def start(self):
         is_true = True
         while is_true:
+
             question = self.next_question()
             print(f"Q.{self.question_number}: {question.get_text()} (True/False): ")
             player_answer  = input().title()
+
             if question.get_answer() == player_answer:
                 print("Right answer")
                 self.score += 1
-                print(f"Your current score is: {self.score}")
+                print(f"Your current score is: {self.score}/{self.question_number}")
             else:
                 print(f"Wrong answer the right answer was {question.get_answer()}")
-                print(f"Your final score is: {self.score}")
-                is_true = False
+                print(f"Your current score is: {self.score}/{self.question_number}")
+            
+
             if self.question_number == len(self.question_list):
-                print("You completed the chalenge!!!")
-                print(f"Your final score is: {self.score}")
+                print("\n")
+                print("You've completed the chalenge!!!")
+                print(f"Your final score is: {self.score}/{self.question_number}")
                 is_true = False
-    
+
+            print("\n")
