b39e2e451cd9efc138712ad2128952a754f3e122
Ariadne Vieira <64850642+Aaaririri@users.noreply.github.com>
2021-04-01 18:35:41 -0300

    Add files via upload

--- 100 Days of Code - Python/Day 024/Mail Merge Project Start/main.py
+++ 100 Days of Code - Python/Day 024/Mail Merge Project Start/main.py
@@ -10,7 +10,6 @@
 
 lines = open("Day 024/Mail Merge Project Start/Input/Names/invited_names.txt", mode ="r")
 names = lines.readlines()
-print(names)
 
 
 letter = open("Day 024/Mail Merge Project Start/Input/Letters/starting_letter.txt", mode ="r")
@@ -22,7 +21,6 @@ for name in names:
     path = f"Day 024/Mail Merge Project Start/Output/ReadyToSend/{txt}.txt"
     with open(path, mode ="w") as text:
         text.write(original_letter.replace(anterior_name, txt))
-        anterior_name = txt
 
 letter.close()
 lines.close()
