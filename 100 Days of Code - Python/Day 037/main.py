b10ae07cc5266e430d7eb113bcb9e74d34c5ebf7
Ariadne Vieira <64850642+Aaaririri@users.noreply.github.com>
2021-04-20 14:43:40 -0300

    Add files via upload

---
+++ 100 Days of Code - Python/Day 037/main.py
@@ -0,0 +1,10 @@
+import requests
+params_pixela = {
+    "token":"senhatopzeira", 
+    "username":"aaaririri", 
+    "agreeTermsOfService":"yes", 
+    "notMinor":"yes", 
+    "thanksCode":"ThisIsThanksCode"}
+
+#response = requests.post("https://pixe.la/v1/users", json = params_pixela)
+
