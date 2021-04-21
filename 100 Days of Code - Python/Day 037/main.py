012dd5c688492819b88cab6cdb27d3f5004a335e
Ariadne Vieira <64850642+Aaaririri@users.noreply.github.com>
2021-04-21 18:02:34 -0300

    Add files via upload

--- 100 Days of Code - Python/Day 037/main.py
+++ 100 Days of Code - Python/Day 037/main.py
@@ -1,10 +1,48 @@
 import requests
+import datetime
+"""
+https://pixe.la/ 
+you need to request your own username and password and your graph's name
+"""
+USERNAME = "aaaririri1"
+PASSWORD = "senhatopzeira1"
 params_pixela = {
-    "token":"senhatopzeira", 
-    "username":"aaaririri", 
+    "token":PASSWORD, 
+    "username":USERNAME, 
     "agreeTermsOfService":"yes", 
-    "notMinor":"yes", 
-    "thanksCode":"ThisIsThanksCode"}
+    "notMinor":"yes"
+    }
 
 #response = requests.post("https://pixe.la/v1/users", json = params_pixela)
+GRAPHID = "graphbike"
+params_graph = {
+    "id":GRAPHID,
+    "name":"Biking",
+    "unit":"Km",
+    "type":"float",
+    "color":"momiji"
+    }
 
+graph_endpoint = "https://pixe.la/v1/users/{USERNAME}/graphs"
+
+headers = {
+    "X-USER-TOKEN":PASSWORD
+}
+
+#response = requests.post(url = graph_endpoint, json = params_graph, headers = headers)
+
+today = datetime.datetime(datetime.date.today().year,datetime.date.today().month, datetime.date.today().day)
+
+pixel_endpoint = "https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPHID}"
+pixel_params = {
+    "date":today.strftime('%Y%m%d'),
+    "quantity":"15"
+}
+#response = requests.post(url = pixel_endpoint, json = pixel_params, headers = headers)
+
+
+update_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPHID}/{pixel_params['date']}"
+
+
+delete_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPHID}/{pixel_params['date']}"
+#response = requests.delete(url = delete_endpoint, headers = headers)
